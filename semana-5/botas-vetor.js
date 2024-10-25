ef5390051c1f85980eac04c89f80c008734c9a9a
Rafael <rafaeldavi8086@gmail.com>
2024-10-25 19:52:55 -0300

    Solução em aula do exercício 14 referente a aula da semana 4

---
+++ semana-5/botas-vetor.js
@@ -0,0 +1,28 @@
+const numeros = [38, 42, 39, 40, 38, 40, 37, 44, 37, 42, 41, 44, 37, 42, 42];
+const pes = "EEEDDDEDDDEEEDE";
+
+const base = 37;
+let esquerdos = new Array(8);
+let direitos = new Array(8);
+esquerdos.fill(0);
+direitos.fill(0);
+
+for (let i = 0; i < numeros.length; i++) {
+  const n = numeros[i];
+  const pe = pes[i];
+
+  const pos = n - base;
+  if (pe == 'E') {
+    esquerdos[pos]++;
+  } else {
+    direitos[pos]++;
+  }
+}
+
+for (let i = 0; i < direitos.length; i++){
+  const min = direitos[i] < esquerdos[i] ? direitos[i] : esquerdos[i];
+  if (min > 0) {
+    const num = base + i;
+    console.log(num, min);
+  }
+}
